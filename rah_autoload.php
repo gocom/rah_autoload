fb01d1e24c9646ea73d87dd86222973750e0c324
Jukka Svahn <web@rahinaa.biz>
2014-03-24 14:09:55 +0200

    Update header comments.

--- rah_autoload.php
+++ rah_autoload.php
@@ -1,15 +1,24 @@
 <?php
 
-/**
- * Rah_autoload plugin for Textpattern CMS.
+/*
+ * rah_autoload - Flat templates for Textpattern CMS
+ * https://github.com/gocom/rah_autoload
  *
- * @author  Jukka Svahn
- * @license GNU GPLv2
- * @link    https://github.com/gocom/rah_autoload
+ * Copyright (C) 2014 Jukka Svahn
  *
- * Copyright (C) 2014 Jukka Svahn http://rahforum.biz
- * Licensed under GNU General Public License version 2
- * http://www.gnu.org/licenses/gpl-2.0.html
+ * This file is part of rah_autoload.
+ *
+ * rah_autoload is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, version 2.
+ *
+ * rah_autoload is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with rah_autoload. If not, see <http://www.gnu.org/licenses/>.
  */
 
 include __DIR__ . '/../../autoload.php';
